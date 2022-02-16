4d3e73da6ec44af63ed7515ccdb333de74611914
Héctor Robles <robletes062901@gmail.com>
2022-02-16 15:54:40 -0600

    first and second point (I think)

--- practices/first/labSys13.m
+++ practices/first/labSys13.m
@@ -1,29 +1,86 @@
+format rational;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%% 6.1, considere las siguientes señales:
-%%%%%%%%%%%%%% crear vector con la variable independiente
+%%%%%%%%%%%%%% create vector to store the independient variable
 t = 0:0.00001:20;
-%%%%%%%%%%%%%% señal 1:
-x1 = 2*cos(10*t+1)-sin(4*t-1);
-%%%%%%%%%%%%%% señal 2:
-x2 = (cos(2*t-pi/3)).^2;
-%%%%%%%%%%%%%% Para la señal 1:
+%%%%%%%%%%%%%% signal 1:
+x1 = 2 * cos( 10 * t + 1 ) - sin( 4 * t - 1 );
+%%%%%%%%%%%%%% signal 2:
+x2 = ( cos( 2 * t - pi / 3 ) ).^2;
+%%%%%%%%%%%%%% To signal 1:
+%%%%%% Periodicity of 2 * cos(10 * t+1):
+periodCos = ( 2 * pi ) / abs( 10 );
+%%%%%% Periodicity of sin(4 * t-1):
+periodSin = ( 2 * pi ) / abs( 4 );
+periodOne = periodCos / periodSin;
+% >> periodOne = 2/5, 2 in Z, 5 in Z :. his period is Pi
+%%%%%%%%%%%%%% To signal 2:
+%%%%%% Periodicity of ( cos( 2 * t - pi / 3 ) ).^2:
+periodTwo = ( 2 * pi ) / abs( 2 / 3 );
+% >> periodTwo = period of the coseno
+%%%%%% signal 1 is periodic :. the local max from a period is the absolute
+%%%%%% max
 max1 = max(x1);
-
+%%%%%% signal 2 is periodic :. the local max from a period is the absolute
+%%%%%% max
+max2 = max(x2);
+%%%%%%%%%%%%%% plot the signals
 plot(t, x1);
-%figure;
-%plot(t, x2);
+hold on;
+plot(t, x2);
 
-%fsi = 246.042;
-%fsol = 195.998;
-%fla = 220;
-%wsi = 2*pi*fsi;
-%wla = 2*pi*fla;
-%wsol = 2*pi*fsol;
-%xsi=sin(wsi*t);
-%xsol=sin(wsol*t);
-%xla=sin(wla*t);
-%xs = xsol+xsi+xla;
-%figure;
-%plot(t, xs);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 6.2 Considere ahora que las notas musicales
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% si, la y sol tienen kas siguientes funciones
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% fundamentales f_si = 246.942 [Hz], f_la = 220
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% [Hz] y f_sol = 195.998 [Hz]. Donde el sonido
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% se puede definir a través del siguiente
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% modelo matemático:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% x = sin( wt + p )
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% donde w es la frecuencia fundamental de cada
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% nota musical. Determine si:
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% x_1 = x_si + x_la + x_sol
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%% es periódica o no.
+%%%%%%%%%%%%%% create vector to store the independient variable
+t = 0:0.01:2;
+%%%%%%%%%%%%%% frequency of si
+f_si = 246.042;
+%%%%%%%%%%%%%% frequency of sol
+f_sol = 195.998;
+%%%%%%%%%%%%%% frequency of la
+f_la = 220;
+%%%%%%%%%%%%%% w of si
+w_si = 2 * pi * f_si;
+%%%%%%%%%%%%%% w of sol
+w_sol = 2 * pi * f_sol;
+%%%%%%%%%%%%%% w of la
+w_la = 2 * pi * f_la;
+%%%%%%%%%%%%%% "sound" of si
+x_si = sin( w_si * t);
+%%%%%%%%%%%%%% "sound" of sol
+x_sol = sin( w_sol * t);
+%%%%%%%%%%%%%% "sound" of la
+x_la = sin( w_la * t);
+%%%%%%%%%%%%%% Sum of all the "sounds"
+xs = x_sol + x_si + x_la;
+%%%%%% Periodicity of w_si:
+periodSi = ( 2 * pi ) / abs( w_si );
+% >> periodSi = 119/29279, 119 in Z, 29279 in Z :. "sound" si is periodic
+%%%%%% Periodicity of w_sol:
+periodSol = ( 2 * pi ) / abs( w_sol );
+% >> periodSol = 500/97999, 500 in Z, 97999 in Z :. "sound" sol is periodic
+%%%%%% Periodicity of w_La:
+periodLa = ( 2 * pi ) / abs( w_la );
+% >> periodLa = 1/220, 1 in Z, 220 in Z :. "sound" la is periodic
+%%%%%% Periodicity of periodSi + periodSol:
+periodSumSiSol = periodSi + periodSol;
+% >> periodSumSiSol = 235/25637, 235 in Z, 25637 in Z :. "sound" si + sol is
+% periodic
+%%%%%% Periodicity of periodSumSiSol + periodla:
+periodSumSiSolLa = periodSumSiSol + periodLa;
+% >> periodSumSiSolLa = 85/6199, 85 in Z, 6199 in Z :. "sound" si + sol +
+% la is periodic
+%%%%%%%%%%%%%% plot the sum
+figure;
+plot(t, xs);
 
 %xsp = (1/2) * (xs+flip(xs));
 %xsimp = (1/2) * (xs-flip(xs));
